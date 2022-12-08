f6343b3e453f33cf5dbd8facaa54766e64ed86bc
최현제 <qwas15788@gmail.com>
2022-12-08 15:25:58 +0900

    22.12.08

---
+++ BaekJoon/Bronze/5576.py
@@ -0,0 +1,11 @@
+w = []
+k = []
+for _ in range(10):
+    w.append(int(input()))
+for _ in range(10):
+    k.append(int(input()))
+
+w.sort(reverse=True)
+k.sort(reverse=True)
+print(w[0]+w[1]+w[2], end=" ")
+print(k[0]+k[1]+k[2])
